0ab3a3d1246d20b8145d1c21033b8bc840c2fbf0
Rohan Kumar <rohaan@redhat.com>
2020-11-10 23:12:29 +0530

    Update Fabric8 Kubernetes Client to v4.13.0

--- src/main/java/io/fabric8/testing/SimpleSharedInformerRun.java
+++ src/main/java/io/fabric8/testing/SimpleSharedInformerRun.java
@@ -4,13 +4,9 @@ import io.fabric8.kubernetes.api.model.Pod;
 import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
-import io.fabric8.kubernetes.client.dsl.base.OperationContext;
 import io.fabric8.kubernetes.client.informers.ResourceEventHandler;
 import io.fabric8.kubernetes.client.informers.SharedIndexInformer;
 import io.fabric8.kubernetes.client.informers.SharedInformerFactory;
-import io.fabric8.testing.crd.PodSet;
-import io.fabric8.testing.crd.PodSetList;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,17 +31,6 @@ public class SimpleSharedInformerRun {
                 PodList.class,
                 RESYNC_PERIOD);
 
-        SharedIndexInformer<PodSet> podSetInformer = informerFactory.sharedIndexInformerForCustomResource(
-                new CustomResourceDefinitionContext.Builder()
-                        .withKind("PodSet")
-                        .withGroup("demo.k8s.io")
-                        .withVersion("v1alpha1")
-                        .withScope("Namespaced")
-                        .build(),
-                PodSet.class,
-                PodSetList.class,
-                RESYNC_PERIOD);
-
         podInformer.addEventHandler(new ResourceEventHandler<Pod>() {
             @Override
             public void onAdd(Pod pod) {
@@ -60,23 +45,6 @@ public class SimpleSharedInformerRun {
             @Override
             public void onDelete(Pod pod, boolean b) {
                 log.info("DELETED: " + pod.getMetadata().getNamespace() + "/" + pod.getMetadata().getName());
-            }
-        });
-
-        podSetInformer.addEventHandler(new ResourceEventHandler<PodSet>() {
-            @Override
-            public void onAdd(PodSet podSet) {
-                log.info("ADDED: " + podSet.getMetadata().getNamespace() + "/" + podSet.getMetadata().getName());
-            }
-
-            @Override
-            public void onUpdate(PodSet podSet, PodSet t1) {
-                log.info("UPDATED: " + podSet.getMetadata().getNamespace() + "/" + podSet.getMetadata().getName());
-            }
-
-            @Override
-            public void onDelete(PodSet podSet, boolean b) {
-                log.info("DELETED: " + podSet.getMetadata().getNamespace() + "/" + podSet.getMetadata().getName());
             }
         });
 
